8735fcbdae865665b553efdfd9274d03fcfb3691
Omer <omerfarukatas96@gmail.com>
2022-07-19 11:51:58 +0300

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+
+import React from 'react'
+
+export default function Keypad() {
+  return (
+    <input type="password" onChange={() => console.log('Entering password...')} />
+  )
+}
